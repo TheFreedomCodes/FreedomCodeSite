a8fc09c4295c2b59a8ea120e42beb8edcd6e98ca
Rejith Jacob <92424210+RJ2601@users.noreply.github.com>
2023-10-04 20:00:08 +0530

    Add files via upload

---
+++ formsubmit.js
@@ -0,0 +1,35 @@
+// Replace 'YOUR_API_ENDPOINT' with the actual endpoint of your FormSubmit API
+const apiEndpoint = 'https://your-api-endpoint.com/submit';
+
+// Define the data you want to send to the API in JSON format
+const formData = {
+  name: 'John Doe',
+  email: 'johndoe@example.com',
+  message: 'Hello, FormSubmit API!'
+};
+
+// Create a request object with the appropriate method, headers, and body
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json' // Specify the content type as JSON
+  },
+  body: JSON.stringify(formData) // Convert the data to JSON format
+};
+
+// Make the API request
+fetch(apiEndpoint, requestOptions)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+    return response.json(); // Parse the response JSON
+  })
+  .then(data => {
+    // Handle the API response data here
+    console.log('API Response:', data);
+  })
+  .catch(error => {
+    // Handle errors here
+    console.error('API Request Error:', error);
+  });
